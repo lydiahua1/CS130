1de5e8f1138f262f39bca9cd48cb277ef1fc8b12
lydiahua2 <103151255+lydiahua1@users.noreply.github.com>
2022-04-27 17:48:20 -0500

    font

--- Tutorials/tutorial05/01-font-switcher/index.js
+++ Tutorials/tutorial05/01-font-switcher/index.js
@@ -1,12 +1,23 @@
+let currentFont= 1.4;
+
 const makeBigger = () => {
-   alert('make bigger!');
+   // alert('make bigger!');
+   currentFont += 0.2;
+   setFont();
 };
 
 const makeSmaller = () => {
-   alert('make smaller!');
+   // alert('make smaller!');
+   currentFont -= 0.2;
+   setFont();
 };
 
-/*
-document.querySelector(???).addEventListener('click', makeBigger);
-document.querySelector(???).addEventListener('click', makeSmaller);
-*/
+const setFont =() => {
+   document.querySelector(".content").style.fontSize = `${currentFont}em`;
+   document.querySelector("h1").style.fontSize = `${currentFont + 0.5}em`;
+};
+
+
+document.querySelector("#a1").addEventListener('click', makeBigger);
+document.querySelector("#a2").addEventListener('click', makeSmaller);
+
--- Tutorials/tutorial05/03-dyslexia-mode/index.js
+++ Tutorials/tutorial05/03-dyslexia-mode/index.js
@@ -2,3 +2,13 @@
   See Smashing Magazine Tutorial:
   https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
 */
+
+const toggle = document.querySelector("#dyslexia-toggle");
+
+const toggleEventHandler = (ev) => {
+  //alert("hh");
+  //console.log("test");
+  document.body.classList.toggle("dyslexia-mode");
+};
+
+toggle.addEventListener("click", toggleEventHandler);
